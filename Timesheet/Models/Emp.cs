2268ac0fa91303f8df4211fced1a870f53f7a6ef
Robert S <rspeight@students.chattahoocheetech.edu>
2018-02-06 05:08:00 -0500

    fdsf

--- Timesheet/Models/Emp.cs
+++ Timesheet/Models/Emp.cs
@@ -14,7 +14,7 @@ namespace Timesheet.Models
     
     public partial class Emp
     {
-        //minor comment
+        //minor comments
         public int EmpId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
